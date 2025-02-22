b071b26ade8d5a7091772000e4c3f9ad92ad87c9
Patel045 <aayushpatel2904@gmail.com>
2025-02-23 01:27:51 +0530

    Added Comments

--- cpu_tracing.cpp
+++ cpu_tracing.cpp
@@ -62,33 +62,27 @@ int main(int argc, char* argv[]){
     std::ofstream img_file("cpu_image.ppm");
 
     // Image
-    auto aspect_ratio = 16.0 / 9.0;
     int image_width = std::atoi(argv[1]);
     int samples_per_pixel = std::atoi(argv[2]);
-
-    // Calculate the image height, and ensure that it's at least 1.
+    auto aspect_ratio = 16.0 / 9.0;
     int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
 
     // Camera
-    auto focal_length = 1.0;
+    auto camera_center = point3(0, 0, 0);
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0, 0, 0);
+    auto focal_length = 1.0;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    // Viewport
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
+    // Output
     color* output_color = new color[image_height*image_width];
     img_file << "P3\n" << image_width << " " << image_height << "\n255\n";
     
